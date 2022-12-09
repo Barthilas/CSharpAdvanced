05729613d009be71c37993290c9d61c403396d7f
Barthilas-WORK <snetivy.martin@seznam.cz>
2022-12-09 19:38:14 +0100

    part 6 wip

---
+++ StackableStateMachineDesignPattern/Engine.cs
@@ -0,0 +1,42 @@
+﻿using StackableStateMachineDesignPattern.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackableStateMachineDesignPattern
+{
+    internal class Engine
+    {
+        public bool IsRunning { get; set; }
+        public Engine()
+        {
+
+        }
+        public void Quit()
+        {
+
+        }
+
+        public void PushState(IEngineState state)
+        {
+
+        }
+
+        public void PopState(IEngineState state)
+        {
+
+        }
+
+        public void SwitchState(IEngineState state)
+        {
+
+        }
+
+        public void ProcessInput(ConsoleKeyInfo key)
+        {
+
+        }
+    }
+}
